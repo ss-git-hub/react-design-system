be38301adfe621dba652e31c39428c81fcbba859
ss-git-hub <saqib.saghir.job@gmail.com>
2025-07-03 15:20:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), svgLoader()],
+	plugins: [react()],
 	css: {
 		preprocessorOptions: {
 			sass: {
@@ -15,10 +15,5 @@ export default defineConfig({
 	test: {
 		environment: 'jsdom',
 		setupFiles: './setupTests.ts',
-	},
-	esbuild: {
-		formatOptions: {
-			prettier: true,
-		},
 	},
 });
